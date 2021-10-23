9efdb0827c0998322aa51b5d7ba90249859b535f
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2021-10-23 18:49:03 -0400

    .

--- P2P_V1/P2P.fsx
+++ P2P_V1/P2P.fsx
@@ -8,20 +8,43 @@ open Akka.FSharp
 open Akka.Configuration
 
 type Message =
-    | A of int
+    | SetTotalNodes of int
     | B of int
     | C of int
 
 let system = ActorSystem.Create("System")
 
 //Basic Actor Structure
-type SomeActor() =
+type ProcessController() =
+    inherit Actor()
+    //Define required variables here
+    let mutable totalNodes = 0
+
+    override x.OnReceive(receivedMsg) =
+        match receivedMsg :?> Message with 
+            | SetTotalNodes nodes ->
+                totalNodes <- nodes
+            | _ -> ()
+
+type Peer() =
     inherit Actor()
     //Define required variables here
 
     override x.OnReceive(receivedMsg) =
-        match receivedMsg :?> Message with 
+        match receivedMsg :?> Message with
             | A int ->
                 ()
             | _ -> ()
 
+//Actual Working starts here
+let numNodes = int (string (fsi.CommandLineArgs.GetValue 1))
+let numRequests = int (string (fsi.CommandLineArgs.GetValue 2))
+
+let processController = system.ActorOf(Props.Create(typeof<ProcessController>),"processController")
+
+//If there needs to be any modification in the number of nodes, please do so here
+
+
+//_______________________________________________________________________________
+
+processController <! SetTotalNodes(numNodes) //Initializing the total number of nodes in the entire system
