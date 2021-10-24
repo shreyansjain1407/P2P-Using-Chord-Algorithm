56035432d94d81b73ed0061d731f2311569755fe
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2021-10-24 15:29:36 -0400

    .

--- P2P_V1/P2P.fsx
+++ P2P_V1/P2P.fsx
@@ -9,38 +9,58 @@ open Akka.Configuration
 
 type Message =
     | SetTotalNodes of int
+    | PeerRing of IActorRef[]
+    | RequestCompletion
+    | Request
     | B of int
     | C of int
 
 let system = ActorSystem.Create("System")
 
 //Basic Actor Structure
-type ProcessController() =
+type ProcessController(nodes: int) =
     inherit Actor()
     //Define required variables here
-    let mutable totalNodes = 0
+    let totalNodes = nodes
+    let mutable completedNodes = 0
 
     override x.OnReceive(receivedMsg) =
         match receivedMsg :?> Message with 
             | SetTotalNodes nodes ->
                 totalNodes <- nodes
+            | RequestCompletion ->
+                completedNodes <- completedNodes + 1
+                if(completedNodes = totalNodes)
+                    printfn "All the nodes have completed the number of requests to be made"
             | _ -> ()
 
-type Peer() =
+type Peer(processController: IActorRef, requests: int) =
     inherit Actor()
     //Define required variables here
+    //HashTable to be defined here
+    let totalRequests = requests
+    let successors = Map.empty<Peer,string>
+    // successors.Add(processController, "23")
+    //Counter to keep track of message requests sent by the given peer
+    let mutable messageRequests = 0
+    while messageRequests < totalRequests do
+        
 
     override x.OnReceive(receivedMsg) =
         match receivedMsg :?> Message with
             | A int ->
                 ()
+            | Request ->
+                if(messageRequests = totlRequests){
+                    processController <! RequestCompletion
+                }
             | _ -> ()
 
 //Actual Working starts here
 let numNodes = int (string (fsi.CommandLineArgs.GetValue 1))
 let numRequests = int (string (fsi.CommandLineArgs.GetValue 2))
 
-let processController = system.ActorOf(Props.Create(typeof<ProcessController>),"processController")
+let processController = system.ActorOf(Props.Create(typeof<ProcessController>, numNodes),"processController")
 
 //If there needs to be any modification in the number of nodes, please do so here
 
@@ -48,3 +68,11 @@ let processController = system.ActorOf(Props.Create(typeof<ProcessController>),"
 //_______________________________________________________________________________
 
 processController <! SetTotalNodes(numNodes) //Initializing the total number of nodes in the entire system
+
+//Initializing the entire ring as an array for now, until further progress
+
+let ring = Array.zeroCreate(numNodes)
+
+for i in [9 .. numNodes] do
+    ring.[i] <- system.ActorOf(Props.Create(typeof<Peer>, processController, numRequests), "Peer" + string i)
+
