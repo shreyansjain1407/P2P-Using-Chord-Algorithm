08e871029673f142903693762f8329712e395525
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2021-10-24 16:41:39 -0400

    .

--- P2P_V1/P2P.fsx
+++ P2P_V1/P2P.fsx
@@ -11,7 +11,7 @@ type Message =
     | SetTotalNodes of int
     | PeerRing of IActorRef[]
     | RequestCompletion
-    | Request
+    | SendRequest
     | B of int
     | C of int
 
@@ -39,20 +39,40 @@ type Peer(processController: IActorRef, requests: int) =
     //Define required variables here
     //HashTable to be defined here
     let totalRequests = requests
-    let successors = Map.empty<Peer,string>
+    let fingerTable = Map.empty
     // successors.Add(processController, "23")
     //Counter to keep track of message requests sent by the given peer
     let mutable messageRequests = 0
+
+    //This while loop needs to be replaced with a scheduling function that automatically
+    //sends a message every second according to the project spec something similar to:
+    //https://www.dotkam.com/2011/10/11/akka-scheduler-sending-message-to-actors-self-on-start/
+    //################################################################################
     while messageRequests < totalRequests do
-        
+        Actor.Context.Self <! SendRequest
+
+
+    Actor.Context.Dispatcher //This is supposed to be used for automated message scheduling
+    //What we can also do is to run a loop in the primary execution space that will implement
+    //a thread.sleep(1s) and then send a message to all of the actors to randomly send requests
+    //this way we will not only have a consistent message sending process where we know how many
+    //messages have been sent but also we don't need to implement a self dispacher and all the actors
+    //can be initialized to the ring of actors beforehand
+    
+    
+    //################################################################################
+    
 
     override x.OnReceive(receivedMsg) =
         match receivedMsg :?> Message with
             | A int ->
                 ()
-            | Request ->
+            | SendRequest ->
                 if(messageRequests = totlRequests){
                     processController <! RequestCompletion
+                } else {
+                    //Send a request for a random peer over here
+                    
                 }
             | _ -> ()
 
