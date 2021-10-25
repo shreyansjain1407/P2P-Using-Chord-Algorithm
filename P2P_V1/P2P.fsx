d2dacc745c3333a3a2bf5104b563cba979a77b62
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2021-10-25 15:53:16 -0400

    Cleaned up Code

--- P2P_V1/P2P.fsx
+++ P2P_V1/P2P.fsx
@@ -41,33 +41,13 @@ type ProcessController(nodes: int) =
 type Peer(processController: IActorRef, requests: int, numNodes: int) =
     inherit Actor()
     //Define required variables here
-    //HashTable to be defined here
     let totalPeers = numNodes
+    //HashTable to be defined here
     let fingerTable = Map.empty
-    // successors.Add(processController, "23")
+
     //Counter to keep track of message requests sent by the given peer
     let mutable messageRequests = 0
 
-    //################################################################################
-    //THIS HAS BEEN IMPLEMENTED AND THE COMMENTS NEED TO BE REMOVED BEFORE SUBMISSION OF THE FILE
-    //################################################################################
-    //This while loop needs to be replaced with a scheduling function that automatically
-    //sends a message every second according to the project spec something similar to:
-    //https://www.dotkam.com/2011/10/11/akka-scheduler-sending-message-to-actors-self-on-start/
-    
-    // while messageRequests < totalRequests do
-    //     Actor.Context.Self <! SendRequest
-
-    // Actor.Context.System.Scheduler.ScheduleTellRepeatedly(....)
-
-    //Actor.Context.Dispatcher 
-    //This is supposed to be used for automated message scheduling
-    //What we can also do is to run a loop in the primary execution space that will implement
-    //a thread.sleep(1s) and then send a message to all of the actors to randomly send requests
-    //this way we will not only have a consistent message sending process where we know how many
-    //messages have been sent but also we don't need to implement a self dispacher and all the actors
-    //can be initialized to the ring of actors beforehand
-    //################################################################################
     override x.OnReceive(receivedMsg) =
         match receivedMsg :?> Message with
             | A int ->
@@ -95,7 +75,6 @@ let numRequests = int (string (fsi.CommandLineArgs.GetValue 2))
 let processController = system.ActorOf(Props.Create(typeof<ProcessController>, numNodes),"processController")
 
 //If there needs to be any modification in the number of nodes, please do so here
-
 
 //_______________________________________________________________________________
 
