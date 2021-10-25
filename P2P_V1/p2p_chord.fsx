96b530f97b7648bb5e69e1e1d866589cc9e8e73f
Marcus Elosegui <Marcus.Elosegui@headquarters.polysmith.com>
2021-10-25 15:48:27 -0400

    second version

---
+++ P2P_V1/p2p_chord.fsx
@@ -0,0 +1,80 @@
+﻿#time "on"
+#r "nuget: Akka.FSharp"
+#r "nuget: Akka.TestKit"
+
+open System
+open Akka.Actor
+open Akka.FSharp
+open Akka.Configuration
+
+type Message = 
+    | SetTotalNodes of int
+    | RequestCompletion of int
+
+type PeerMessage =
+    | Init of int
+    | SendRequest of int
+
+let system = ActorSystem.Create("System")
+
+
+type ProcessController(nodes : int) =
+    inherit Actor()
+    let mutable totalNodes = nodes
+    let mutable completedNodes = 0
+    let mutable numHops = 0
+
+    override x.OnReceive(receivedMsg) =
+        match receivedMsg :?> Message with
+            | SetTotalNodes nodes ->
+                totalNodes <- nodes
+            | RequestCompletion hops ->
+                completedNodes <- completedNodes + 1
+                numHops <- numHops + hops
+                if(completedNodes = totalNodes) then
+                    let avgHops = (float numHops) / (float totalNodes)
+                    printfn "All the nodes have completed the number of requests to be made"
+                    printfn "Average number of hops: %.1f" avgHops
+            | _ -> ()
+
+
+
+
+
+type Peer(ProcessController : IActorRef, requests : int) =
+    inherit Actor()
+    let totalRequests = requests
+    let mutable nodeID = 0
+    let mutable messageRequests = 0
+
+    override x.OnReceive(receivedMsg) = 
+        match receivedMsg :?> PeerMessage with
+            | Init id ->
+                nodeID <- id
+            | SendRequest requestID ->
+                    
+            | _ -> ()
+    
+
+
+//Actual Working starts here
+let numNodes = int (string (fsi.CommandLineArgs.GetValue 1))
+let numRequests = int (string (fsi.CommandLineArgs.GetValue 2))
+
+let processController = system.ActorOf(Props.Create(typeof<ProcessController>, numNodes),"processController")
+
+//If there needs to be any modification in the number of nodes, please do so here
+
+
+//_______________________________________________________________________________
+
+processController <! SetTotalNodes(numNodes) //Initializing the total number of nodes in the entire system
+
+//Initializing the entire ring as an array for now, until further progress
+
+let ring = Array.zeroCreate(numNodes)
+
+for i in [9 .. numNodes] do
+    ring.[i] <- system.ActorOf(Props.Create(typeof<Peer>, processController, numRequests), "Peer" + string i)
+for i in [9 .. numNodes] do
+    ring.[i] <! Init(i)
